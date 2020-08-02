ffc6e3fc57f23247aa5fb9bc7bfda376fb3e32f9
Maxwell Warsaw <Max585t@gmail.com>
2020-08-02 00:51:31 -0600

    updated recipe object

--- app/src/main/java/com/warsaw/pantrypal/Recipe.java
+++ app/src/main/java/com/warsaw/pantrypal/Recipe.java
@@ -1,4 +1,17 @@
 package com.warsaw.pantrypal;
 
+import java.util.Map;
+
 public class Recipe {
+    String name;
+    String[] intructions;
+    Map<String, Food> ingredients;
+    float time;
+    int numSteps;
+    String comments;
+    String notes;
+
+    public Recipe(){
+        
+    }
 }
