921118cff10b633c5ed61f3cb6f6d05186a4d4eb
Maxwell Warsaw <Max585t@gmail.com>
2020-08-04 16:23:12 -0600

    updated recipe object: added getters and setters

--- app/src/main/java/com/warsaw/pantrypal/Recipe.java
+++ app/src/main/java/com/warsaw/pantrypal/Recipe.java
@@ -1,17 +1,73 @@
 package com.warsaw.pantrypal;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Recipe {
-    String name;
-    String[] intructions;
-    Map<String, Food> ingredients;
-    float time;
-    int numSteps;
-    String comments;
-    String notes;
+    public String name;
+    public String[] instructions = new String[50];
+    public Map<String, Food> ingredients = new HashMap<String, Food>();
+    public float time;
+    private int numSteps;
+    public String comments; //maybe make comments and notes a list of strings
+    public String notes;
 
-    public Recipe(){
-        
+    public Recipe(String name, String[] instructions, Map<String, Food> ingredients, float time, int numSteps, String comments, String notes){
+        this.name = name;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+        this.time = time;
+        this.numSteps = numSteps;
+        this.comments = comments;
+        this.notes = notes;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public void addInstruction(String newInstruction){
+        numSteps++;
+        instructions[numSteps] = newInstruction;
+    }
+
+    public void addIngredient (Food newIngredient){
+        ingredients.put(newIngredient.name, newIngredient);
+    }
+
+    public void setTime(int newTime){
+        time = newTime;
+    }
+
+    public void setComments(String newComments){
+        comments = newComments;
+    }
+
+    public void setNotes(String newNotes){
+        notes = newNotes;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String[] getInstructions() {
+        return instructions;
+    }
+
+    public Map<String, Food> getIngredients() {
+        return ingredients;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public String getNotes() {
+        return notes;
     }
 }
