ccfc7fbdb22cf809abd43a010b733137a3ad5c54
Maxwell Warsaw <Max585t@gmail.com>
2020-08-04 16:32:48 -0600

    updated recipeBook class with getters, setters, and an addRecipe function

--- app/src/main/java/com/warsaw/pantrypal/RecipeBook.java
+++ app/src/main/java/com/warsaw/pantrypal/RecipeBook.java
@@ -1,4 +1,24 @@
 package com.warsaw.pantrypal;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecipeBook {
+    private Map<String, Recipe> recipeBook = new HashMap<String, Recipe>();
+
+    public RecipeBook(Map<String, Recipe> recipeBook){
+        this.recipeBook = recipeBook;
+    }
+
+    public Map<String, Recipe> getRecipeBook() {
+        return recipeBook;
+    }
+
+    public void setRecipeBook(Map<String, Recipe> recipeBook) {
+        this.recipeBook = recipeBook;
+    }
+
+    public void addRecipe(Recipe newRecipe){
+        recipeBook.put(newRecipe.name, newRecipe);
+    }
 }
